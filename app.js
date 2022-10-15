9e8a8ef66bf67cc49e49575e63b468d3c99ddf26
Kostya <leap74rus@gmail.com>
2022-10-15 18:38:52 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const container = document.querySelector('.container');
+
+const upBtn = document.querySelector('.up-button');
+const downBtn = document.querySelector('.down-button');
+
+const sidebar = document.querySelector('.sidebar');
+const mainSlide = document.querySelector('.main-slide');
+const slidesCoutn = mainSlide.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+sidebar.style.top = `-${(slidesCoutn - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => {
+  changeSlide('up');
+})
+
+downBtn.addEventListener('click', () => {
+  changeSlide('down');
+})
+
+function changeSlide(direction) {
+  if (direction === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex === slidesCoutn) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCoutn - 1;
+    }
+  }
+
+  const height = container.clientHeight
+
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
