c32685eec794dd2b0557039b7c7b5a5a0ab2dcaf
Kostya <leap74rus@gmail.com>
2022-10-15 20:31:47 +0300

    add keyboard arrow

--- app.js
+++ app.js
@@ -12,28 +12,36 @@ let activeSlideIndex = 0;
 sidebar.style.top = `-${(slidesCoutn - 1) * 100}vh`;
 
 upBtn.addEventListener('click', () => {
-  changeSlide('up');
+    changeSlide('up');
 })
 
 downBtn.addEventListener('click', () => {
-  changeSlide('down');
+    changeSlide('down');
+})
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up');
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down');
+    }
 })
 
 function changeSlide(direction) {
-  if (direction === 'up') {
-    activeSlideIndex++;
-    if (activeSlideIndex === slidesCoutn) {
-      activeSlideIndex = 0;
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex === slidesCoutn) {
+            activeSlideIndex = 0;
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCoutn - 1;
+        }
     }
-  } else if (direction === 'down') {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = slidesCoutn - 1;
-    }
-  }
 
-  const height = container.clientHeight
+    const height = container.clientHeight
 
-  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
-  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
 }
